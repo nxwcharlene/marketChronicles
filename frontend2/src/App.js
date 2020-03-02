e6d9a671b0b0efa2dcf53f50cc71615e5341a9a7
Charlene Neo <charlene.nxw@gmail.com>
2020-03-02 12:55:04 +0800

    test commit

---
+++ frontend2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+        <h1>Market Chronicles</h1>
+        <div style={{backgroundColor: "#375259"}}>Test</div>
+        <p><div style={{backgroundColor: "#cccecf"}}>Welcome to our application</div> </p>
+        <p>This is our app</p>
+    </div>
+  );
+}
+
+export default App;
