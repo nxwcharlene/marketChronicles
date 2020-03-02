ba3426894da708398d43addd5bb213cc472941e3
Charlene Neo <charlene.nxw@gmail.com>
2020-03-02 10:07:50 +0800

    test commit

--- macro/views.py
+++ macro/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# CREATE YOUR VIEWS HERE:
+
+# 1: create an index function that will take in a request and return an HTTP response
+def index(request):
+    return HttpResponse("Hello World! This is the macro views index.")
