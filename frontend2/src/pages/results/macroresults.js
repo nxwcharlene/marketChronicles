ceb0672b7f8b5ed6daf7d878bb32bfd0f0f82713
cheklin <tcheklin@gmail.com>
2020-03-06 15:04:07 +0800

    added results section to macro page part 4

--- frontend2/src/pages/results/macroresults.js
+++ frontend2/src/pages/results/macroresults.js
@@ -3,6 +3,9 @@
 
 import React from 'react';
 
+//API_URL created on django side
+const API_URL = 'http://localhost:8000';
+
 /**
  * App
  *
--- macro/models.py
+++ macro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Instrument(models.Model):
+    date = models.CharField(max_length=255)
+    open = models.CharField(max_length=255)
+    high = models.CharField(max_length=255)
+    low = models.CharField(max_length=255)
+    close = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name
