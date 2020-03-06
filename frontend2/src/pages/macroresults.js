c63cbcec355b5fb131ac593dccb4afa72a431045
cheklin <tcheklin@gmail.com>
2020-03-06 13:16:42 +0800

    added results section to macro page part 3

--- frontend2/src/pages/macroresults.js
+++ frontend2/src/pages/macroresults.js
@@ -1,37 +1,54 @@
 //get output from backend via API
 // display JSON, have to convert to Bokeh
 
-import React, {component} from "react"
-
-class MacroResult extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            resultsList: null
-        };
-    }
-
-    componentDidMount() {
-        // Simple GET request using fetch, using random API as placeholder
-        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=0H3OLF9PWVUMK66F')
-            .then(response => response.json())
-            .then(data => this.setState({ resultsList: data.total }));
-    }
-
-    render() {
-        const { resultsList } = this.state;
-        return (
-            <div className="card text-center m-3">
-                <div className="card-body">
-                    Past Instances: {resultsList}
-                </div>
-            </div>
-        );
-    }
-}
-
-export default MacroResult
+//import React, {component} from "react"
+//
+//class MacroResult extends React.Component {
+//    constructor(props) {
+//        super(props);
+//
+//        this.state = {
+//            resultsList: [],
+//            isLoaded: false
+//        };
+//    }
+//
+//    componentDidMount() {
+//        // Simple GET request using fetch, using random API as placeholder
+//        fetch('https://jsonplaceholder.typicode.com/users')
+//            .then(response => response.json())
+//            .then(json =>
+//                this.setState({
+//                    resultsList: json,
+//                    isLoaded: true,
+//                })
+//            }).catch((err) => {
+//                console.log(err);
+//            });
+//    }
+//
+//    render() {
+//
+//        const { isLoaded, resultsList } = this.state;
+//
+//        if(!isLoaded)
+//            return <div>Loading...</div>;
+//
+//        return (
+//            <div className="card-body">
+//                <ul>
+//                    {resultsList.map(item => (
+//                        <li key={item.id}>
+//                            Name: {item.name}| Email: {item.email}
+//                        </li>
+//                    ))};
+//                </ul>
+//            </div>
+//        );
+//    }
+//}
+//
+//export default MacroResult
 
 
 // API with set headers at the back
@@ -42,3 +59,66 @@ export default MacroResult
 //        .then(response => response.json())
 //        .then(data => this.setState({ totalReactPackages: data.total }));
 //}
+
+import React from 'react';
+
+/**
+ * App
+ *
+ * Simple react js fetch example
+ */
+class resultsList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    /**
+     * componentDidMount
+     *
+     * Fetch json array of objects from given url and update state.
+     */
+    componentDidMount() {
+
+        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=0H3OLF9PWVUMK66F')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true,
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+//render UI
+    render() {
+
+        const { isLoaded, items } = this.state;
+
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+            <div>
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            Date: {item.id}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+
+    }
+
+}
+
+export default resultsList;
