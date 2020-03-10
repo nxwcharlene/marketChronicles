3c5ef96b8e258899d10dbd6dcf6f91e4a57e1a4d
cheklin <tcheklin@gmail.com>
2020-03-10 09:52:48 +0800

    Restructured searchbar components into individual js files

---
+++ frontend2/src/pages/SearchBar/IndicatorBox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+export default function ComboBox() {
+  return (
+    <Autocomplete
+      id="combo-box-demo"
+      options={top100Films}
+      getOptionLabel={option => option.title}
+      style={{ width: 300 }}
+      renderInput={params => <TextField {...params} label="Combo box" variant="outlined" />}
+    />
+  );
+}
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+ ];
