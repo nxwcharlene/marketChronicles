3b9670aaabffdaa44efec6c32f0eb3fc53b7897a
Charlene Neo <charlene.nxw@gmail.com>
2020-03-23 20:19:28 +0800

    Date Picker on all 3 forms

--- frontend/src/pages/earningspage.js
+++ frontend/src/pages/earningspage.js
@@ -4,7 +4,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import 'react-inputs-validation/lib/react-inputs-validation.min.css';
 // for the user input form
 import EarnAppBar from '../components/structure/navbar/EarnAppBar.js';
-import Form from '../components/userform/formtest.js';
 import EarnForm from '../components/userform/EarnForm.js';
 import Footer from '../components/structure/footer/Footer.js';
 
@@ -26,29 +25,7 @@ function EarningsPage() {
                     <div style={{height:10}}/>
                     <hr></hr>
                     <h3>Sample Results Output</h3>
-                    <div style={{height:500}}/>
-                    <form>
-                        <label>
-                            Company:
-                            <input type="text" name="name" />
-                        </label>
-                        <input type="submit" value="Submit" />
-                        <div style={{height:10}}/>
-                        <label>
-                            Select a company:
-                                <select>
-                                <option value="AAPL">AAPL</option>
-                                <option value="MSFT">MSFT</option>
-                                <option value="GOOGL">GOOGL</option>
-                                </select>
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
-
-                <div style={{height:5}}/>
-                <h1> Testing form submission </h1>
-                <Form />
-                <div style={{height:30}}/>
+                    <div style={{height:400}}/>
                 </Container>
 
                 <Footer />
