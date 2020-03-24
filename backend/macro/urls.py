a17582b6645fdc678ad2d84cbe56a29301b95a55
Charlene Neo <charlene.nxw@gmail.com>
2020-03-24 10:37:06 +0800

    backend urls.py

--- backend/macro/urls.py
+++ backend/macro/urls.py
@@ -4,4 +4,5 @@ from . views import get_macro
 
 urlpatterns = [
     path('macro-get/', views.get_macro, name='get_macros'),
+    path('macro/', views.get_macro, name='macros'),
 ]
--- backend/marketChronicles/urls.py
+++ backend/marketChronicles/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('macro/', include('macro.urls')),
     path('', include('macro.urls')),
+    path('/', include('macro.urls')),
+    path('macro/macro-get/', include('macro.urls')),
 ]
