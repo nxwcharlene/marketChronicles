e57b2d3bda76799c9d29394a2e81c2446a7d419e
Charlene Neo <charlene.nxw@gmail.com>
2020-03-24 11:35:48 +0800

    backend urls.py

--- backend/earnings/views.py
+++ backend/earnings/views.py
@@ -34,57 +34,6 @@ def apiOverview(request):
 
 @api_view(['GET', 'POST'])
 def get_earnings(request):
-    # macro=Macro.objects.filter(ticker='NAPMPMI Index') #replace with indicatorname
-    # df=pd.DataFrame(list(macro.values())) #convert model data to dataframe
-    # df['date'] = pd.to_datetime(df['date'], format='%Y/%m/%d')
-
-    # class user_input: #pending user input from frontend
-    #     def __init__(self, instrument, indicator, surprise_sign_input, surprise_magnitude):
-    #         self.instrument = instrument  # to link to drop down
-    #         self.indicator = indicator  # to link to autofill
-    #         self.surprise_sign_input = surprise_sign_input  # to link to drop down (exceed, meet, below expections)
-    #         self.surprise_magnitude = surprise_magnitude  # to link to drop down (large, medium, small), if user chooses 'meet', cannot choose magnitude
-    # john=user_input("Apple","ISM Manufacturing","Exceed","Small")
-
-    # def surprise_sign_calc(row):
-    #   if float(row['actual'])-float(row['survm'])>0:
-    #       return('Exceed')
-    #   elif float(row['actual'])-float(row['survm'])<0:
-    #       return('Below')
-    #   else:
-    #       return('Meet')
-
-    # df['surprise_sign'] = df.apply(surprise_sign_calc, axis=1)  # create new column for surprise sign
-
-    # def surprise_magnitude_calc(row):
-    #   if abs((float(row['actual'])-float(row['survm']))/float(row['stddev']))>2:
-    #       return('Large')
-    #   elif abs((float(row['actual'])-float(row['survm']))/float(row['stddev']))<1:
-    #       return('Small')
-    #   else:
-    #       return('Medium')
-
-    # df['surprise_magnitude'] = df.apply(surprise_magnitude_calc, axis=1)  # creates new column for surprise magnitude
-
-    # if john.surprise_sign_input != "Meet": #replace with input
-    #     df_results=df[df['surprise_sign'].str.contains(john.surprise_sign_input)&
-    #               df['surprise_magnitude'].str.contains(john.surprise_magnitude)]
-    # else:
-    #     df_results=df[df['surprise_sign'].str.contains(john.surprise_sign_input)]
-
-    # list_of_results = df_results['date']
-    # df['data'] = list_of_results
-    # jsonized_df=df.to_json(date_format='iso',orient="index")
-
-    #### Jeffrey test ####
-    # data = serializers.serialize(format='json', queryset=macro)
-
-    # return HttpResponse(content=data, content_type='application/json')
-    # data = {
-    #     "date" : serialized_class.data.date
-    # }
-    # serialized_class.is_valid()
-    #### Jeffrey Test ####
     ticker = request.GET.get('ticker', 1)
 
     # macro = Macro.objects.filter(id=ticker, date__range=["2011-01-01", "2011-01-31"])
