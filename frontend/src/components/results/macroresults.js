cd2041d7fed82588cba2c2537a21a9b4ac43aab1
cheklin <tcheklin@gmail.com>
2020-03-26 15:30:12 +0800

    added axios.get in frontend macroresults.js

--- frontend/src/components/results/macroresults.js
+++ frontend/src/components/results/macroresults.js
@@ -9,119 +9,147 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import demo from '../../images/demo_results.jpeg';
+import macroresultsAPI from './macroresultsAPI';
 // import { useTable, useGroupBy, useFilters, useSortBy, useExpanded, usePagination } from 'react-table';
 
 // React js fetch: Use componentDidMount to fetch json array of objects from given url and update state
 
+// class MacroResults extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             items: [],
+//             isLoaded: false
+//         }
+//     }
+//     componentDidMount() {
+//         fetch('http://localhost:8000/macro')
+//             .then(res => res.json())
+//             .then(json => {
+//                 this.setState({
+//                     items: json,
+//                     isLoaded: true,
+//                 })
+//             }).catch((err) => {
+//                 console.log(err);
+//             });
+//     }
 
-class MacroResults extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [],
-            isLoaded: false
-        }
-    }
-    componentDidMount() {
-        fetch('http://localhost:8000/macro')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    items: json,
-                    isLoaded: true,
-                })
-            }).catch((err) => {
-                console.log(err);
-            });
-    }
 
+const MacroResults = () => {
+    const [result, setResult] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        const fetchResult = await macroresultsAPI.get('/');
+        setResult(fetchResult.data);
+        setIsLoading(false);
+      }
+  
+      fetchData();
+    }, []);
+  
+    const loadingMessage = () => {
+      return <span>Loading...</span>
+    }
+  
+    //output as list first, haven't format as card yet
+    const data = () => {
+      return result.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))
+    }
+  }
+
+export default MacroResults
 //render UI
-    render() {
+//     render() {
 
-        const { isLoaded, items } = this.state;
+//         const { isLoaded, items } = this.state;
 
-        if (!isLoaded)
-            return <div>Loading...</div>;
+//         if (!isLoaded)
+//             return <div>Loading...</div>;
 
-        return (
-            <div>
-                {items.map(item => (
-//                  <div style={{height:210}}>
-                      <Card style={{marginBottom:15}}>
-                        <CardHeader
-                            title={item.date}
-                            titleTypographyProps={{ align: 'left', variant:'body1'}}
-                            style={{
-                            backgroundColor: 'grey',
-                            height: 20}}
-                        />
+//         return (
+//             <div>
+//                 {items.map(item => (
+// //                  <div style={{height:210}}>
+//                       <Card style={{marginBottom:15}}>
+//                         <CardHeader
+//                             title={item.date}
+//                             titleTypographyProps={{ align: 'left', variant:'body1'}}
+//                             style={{
+//                             backgroundColor: 'grey',
+//                             height: 20}}
+//                         />
 
-                        <CardContent>
-                            <Grid container spacing={0} alignItems="flex-end">
-                                <Grid item key={item.id} xs={3} md={3} style ={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
-                                  <ul>
-                                        <div style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
-                                        <li >
-                                            <b><span>Economic Release </span></b>
-                                        </li>
-                                        <li >
-                                            <span>Ticker: {item.ticker}</span>
-                                        </li>
-                                        <li >
-                                            <span>Indicator: {item.event}</span>
-                                        </li>
-                                        <li >
-                                            <span>Actual: {item.actual}</span>
-                                        </li>
-                                        <li >
-                                            <span>Estimate: {item.survm}</span>
-                                        </li>
-                                        <li >
-                                            <span>Surprise: {item.surprise}</span>
-                                        </li>
-                                        </div>
-                                  </ul>
-                                </Grid>
+//                         <CardContent>
+//                             <Grid container spacing={0} alignItems="flex-end">
+//                                 <Grid item key={item.id} xs={3} md={3} style ={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
+//                                   <ul>
+//                                         <div style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
+//                                         <li >
+//                                             <b><span>Economic Release </span></b>
+//                                         </li>
+//                                         <li >
+//                                             <span>Ticker: {item.ticker}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span>Indicator: {item.event}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span>Actual: {item.actual}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span>Estimate: {item.survm}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span>Surprise: {item.surprise}</span>
+//                                         </li>
+//                                         </div>
+//                                   </ul>
+//                                 </Grid>
 
-                                <Grid item key={item.id} xs={3} md={3}>
-                                    <ul>
-                                        <li >
-                                            <b><span>Post-release returns </span></b>
-                                        </li>
-                                        <li >
-                                            <span> 1 Day: {item.ticker}</span>
-                                        </li>
-                                        <li >
-                                            <span> 1 Week: {item.event}</span>
-                                        </li>
-                                        <li >
-                                            <span> 1 Month: {item.surprise_sign}</span>
-                                        </li>
-                                        <li >
-                                            <span> 3 Months: {item.surprise_magnitude}</span>
-                                        </li>
-                                        <li >
-                                            <span> 6 Months: {item.surprise_magnitude}</span>
-                                        </li>
-                                    </ul>
-                                </Grid>
+//                                 <Grid item key={item.id} xs={3} md={3}>
+//                                     <ul>
+//                                         <li >
+//                                             <b><span>Post-release returns </span></b>
+//                                         </li>
+//                                         <li >
+//                                             <span> 1 Day: {item.ticker}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span> 1 Week: {item.event}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span> 1 Month: {item.surprise_sign}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span> 3 Months: {item.surprise_magnitude}</span>
+//                                         </li>
+//                                         <li >
+//                                             <span> 6 Months: {item.surprise_magnitude}</span>
+//                                         </li>
+//                                     </ul>
+//                                 </Grid>
 
-                                <Grid item key={item.id} xs={6} md={3}>
-                                    <img src={demo} style={{height: 120}} alt=""/>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                      </Card>
-//                  </div>
+//                                 <Grid item key={item.id} xs={6} md={3}>
+//                                     <img src={demo} style={{height: 120}} alt=""/>
+//                                 </Grid>
+//                             </Grid>
+//                         </CardContent>
+//                       </Card>
+// //                  </div>
 
-                ))}
-            </div>
+//                 ))}
+//             </div>
 
-        );
-    }
-}
+//         );
+//     }
+// }
 
-export default MacroResults;
+// export default MacroResults;
 
 
 
