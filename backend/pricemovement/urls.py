a6c918c08beeaecac735d8a7973001603f554d84
chewyuren <yuren.chew@gmail.com>
2020-03-30 11:39:01 +0800

    Merge branch 'master' of https://github.com/nxwcharlene/marketChronicles into yuren
    # Conflicts:
    #	backend/pricemovement/urls.py

--- backend/pricemovement/urls.py
+++ backend/pricemovement/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 from . views import get_news
-from . views import get_price
+
 
 
 urlpatterns = [
-    path('', views.get_news, name='get_news'),
+    path('', views.get_price, name='get_price'),
     path('get-news/', views.get_news, name='get_news'),
 ]
