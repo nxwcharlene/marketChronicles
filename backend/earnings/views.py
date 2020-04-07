35e9b78b3fd081d096337bdc1b3e988a4a593211
leecyg1 <59825743+leecyg1@users.noreply.github.com>
2020-04-07 14:25:52 +0800

    earnings logic

--- backend/earnings/views.py
+++ backend/earnings/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render
-
-# Create your views here.
-#Look at the macro code. Call earnings instead of macro
+# Django Libraries
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from earnings.models import Earnings, Macro, Stockprice, StockId
-import pandas as pd
-from macro.serializer import MacroSerializer
+
+# RestFramework
 from rest_framework import generics
-# from django_pandas.io import read_frame
-import quandl
-quandl.ApiConfig.api_key='dFvSTC2myD1ts7eJq8VD'
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+# Third party Libraries
+import pandas as pd
+import quandl
+
+# Local Libraries
+from earnings.models import Earnings, Macro, Stockprice, StockId
+
+# Python Libraries
 import json
 from datetime import timedelta
 
-from .serializer import EarningsSerializer
 # CREATE YOUR VIEWS HERE:
+
+quandl.ApiConfig.api_key='dFvSTC2myD1ts7eJq8VD'
 
 @api_view(['GET'])
 def apiOverview(request):
@@ -31,10 +34,94 @@ def apiOverview(request):
 def get_id(request):
     return()
 
+@api_view(['GET', 'POST'])
 def get_earnings(request):
-    idframe=StockId.objects.filter(ticker='NAPMPMI Index') #replace with stock_id
-    df=pd.DataFrame(list(idframe.values())) #convert model data to dataframe
-    stockidval=df.at[0,"stock_id"]
+
+    # Get Earnings
+    # Get Surprise Direction and Magnitude
+
+    stock_id = request.GET.get('stock_id', 1)
+    magn=request.GET.get("magn","Pve") #positive
+    expr = request.GET.get('expr', 'small') #For the magnitude
+
+    if expr == 'large' and magn=="Pve":
+        earning = Earnings.objects.filter(stock_id=stock_id).filter(z_score__gt = 2)#greater than
+        stockPrice = Stockprice.objects.filter(stock_id=stock_id)
+    elif expr=="medium" and magn=="Pve":
+        earning = Earnings.objects.filter(stock_id=stock_id).filter(z_score__range = [1,1.9999])#greater than
+        stockPrice = Stockprice.objects.filter(stock_id=stock_id)
+    elif expr=="small" and magn=="Pve":
+        earning = Earnings.objects.filter(stock_id=stock_id).filter(z_score__range = [0.01,0.9999])#greater than
+        stockPrice = Stockprice.objects.filter(stock_id=stock_id)
+    elif expr=="large" and magn=="Nve":
+        earning = Earnings.objects.filter(stock_id=stock_id).filter(z_score__lt = -2)#greater than
+        stockPrice = Stockprice.objects.filter(stock_id=stock_id)
+    elif expr=="medium" and magn=="Nve":
+        earning = Earnings.objects.filter(stock_id=stock_id).filter(z_score__range = [-1.999,-1])#greater than
+        stockPrice = Stockprice.objects.filter(stock_id=stock_id)
+    elif expr=="small" and magn=="Nve":
+        earning = Earnings.objects.filter(stock_id=stock_id).filter(z_score__range = [-0.9999,-0.01])#greater than
+        stockPrice = Stockprice.objects.filter(stock_id=stock_id)
+
+    content = []
+    contentStockPrice = []
+    prices=[]
+    for test in earning.values():
+
+        contentStockPrice.append(test)
+    for price in stockPrice.values() :
+        prices.append(price)
+    pricedict={}
+    for i in prices:
+        pricedict[i["date"]]=i["price"]
+    responselist=[]
+    for i in contentStockPrice:
+        fillerlist=[]
+        try:
+            fillerlist.append(("The one day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=1)]-pricedict[i["date"]],"The 20 day stock price change was",pricedict[i["date"]+timedelta(days=20)]-pricedict[i["date"]],"and the 100 day price stock price change was ",pricedict[i["date"]+timedelta(days=100)]-pricedict[i["date"]]))
+            responselist.append(fillerlist)
+        except KeyError:
+            try:
+                fillerlist.append(("The one day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=1)]-pricedict[i["date"]],"The 20 day stock price change was",pricedict[i["date"]+timedelta(days=20)]-pricedict[i["date"]],"and the 103 day price stock price change was ",pricedict[i["date"]+timedelta(days=103)]-pricedict[i["date"]]))
+                responselist.append(fillerlist)
+            except KeyError:
+                try:
+                    fillerlist.append(("The one day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=1)]-pricedict[i["date"]],"The 20 day stock price change was",pricedict[i["date"]+timedelta(days=20)]-pricedict[i["date"]]))
+                    responselist.append(fillerlist)
+                except KeyError:
+                        try:
+                            fillerlist.append(("The one day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=1)]-pricedict[i["date"]],"The 23 day stock price change was",pricedict[i["date"]+timedelta(days=23)]-pricedict[i["date"]]))
+                            responselist.append(fillerlist)
+                        except KeyError:
+                            try:
+                                fillerlist.append(("The one day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=1)]-pricedict[i["date"]],))
+                                responselist.append(fillerlist)
+                            except KeyError: #start 3 day here
+                                try:
+                                    fillerlist.append(("The 3 day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=3)]-pricedict[i["date"]],"The 20 day stock price change was",pricedict[i["date"]+timedelta(days=20)]-pricedict[i["date"]],"and the 100 day price stock price change was ",pricedict[i["date"]+timedelta(days=100)]-pricedict[i["date"]]))
+                                    responselist.append(fillerlist)
+                                except KeyError:
+                                    try:
+                                        fillerlist.append(("The 3 day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=3)]-pricedict[i["date"]],"The 20 day stock price change was",pricedict[i["date"]+timedelta(days=20)]-pricedict[i["date"]],"and the 103 day price stock price change was ",pricedict[i["date"]+timedelta(days=103)]-pricedict[i["date"]]))
+                                        responselist.append(fillerlist)
+                                    except KeyError:
+                                        try:
+                                            fillerlist.append(("The 3 day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=3)]-pricedict[i["date"]],"The 20 day stock price change was",pricedict[i["date"]+timedelta(days=20)]-pricedict[i["date"]]))
+                                            responselist.append(fillerlist)
+                                        except KeyError:
+                                                try:
+                                                    fillerlist.append(("The 3 day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=3)]-pricedict[i["date"]],"The 23 day stock price change was",pricedict[i["date"]+timedelta(days=23)]-pricedict[i["date"]]))
+                                                    responselist.append(fillerlist)
+                                                except KeyError:
+                                                        fillerlist.append(("The 3 day stock price change on ",i["date"], "was ",pricedict[i["date"]+timedelta(days=3)]-pricedict[i["date"]],))
+                                                        responselist.append(fillerlist)
+
+    return Response(data=responselist)
+
+
+    '''df=pd.DataFrame(list(idframe.values())) #convert model data to dataframe
+
+    stockidval=df.at[1,"stock_id"]
     earnings=Earnings.objects.filter(stock_id=stockidval) #replace with stock_id
     df=pd.DataFrame(list(earnings.values())) #convert model data to dataframe
     df['date'] = pd.to_datetime(df['date'], format='%Y/%m/%d')
@@ -90,7 +177,7 @@ def get_earnings(request):
 
     list_of_results = df_results['date']
     df['data'] = list_of_results
-    jsonized_df=df.to_json(date_format='iso',orient="index")
+    jsonized_df=df.to_json(date_format='iso',orient="index")'''
 
     # ticker = request.GET.get('ticker', 1)
 
